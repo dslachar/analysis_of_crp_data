3cbd6020091f3c6206bfaacf217e051f4251d399
Dave Palazzo <david.palazzo4@du.edu>
2019-02-24 15:29:00 -0700

    Pac cleaning function

--- cleanUp.py
+++ cleanUp.py
@@ -1,1 +1,22 @@
 # Clean up for CRP Data
+import pandas as pd
+
+def cleanPac(filename):
+    '''Cleans pacs txt file
+        Parameters: 1) filename: txt file from CRP
+        Returns: 1) Pandas dataframe of cleaned data '''
+    # read in file and add column names
+    pac = pd.read_csv(filename, sep='\|,\|', header=None,engine='python')
+    pac.columns = ["Cycle", "FECRecNo","PACID","CID","Type","DI","FECCandID"]
+    # Clean cid, amount, date, realcode
+    df = pac['CID'].apply(lambda x: pd.Series(str(x).split(',')))
+    pac['CID'] = df[0]
+    pac.insert(4,'Amount',df[1])
+    pac.insert(5,'Date',df[2])
+    pac.insert(6, 'RealCode',df[3])
+    # Remove vertical bars
+    pac['Cycle'] = pac['Cycle'].str.replace('|','')
+    pac['CID'] = pac['CID'].str.replace('|','')
+    pac['RealCode'] = pac['RealCode'].str.replace('|','')
+    pac['FECCandID'] = pac['FECCandID'].str.replace('|','')
+    return pac
