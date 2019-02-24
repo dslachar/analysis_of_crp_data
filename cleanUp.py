ecf787288b51a27faf332dd117f0b60917387391
David LaCharite <dslachar@gmail.com>
2019-02-24 14:22:10 -0700

    add pac to pac cleanup function

--- cleanUp.py
+++ cleanUp.py
@@ -1,1 +1,12 @@
 # Clean up for CRP Data
+
+def clean_pac_2_pac(data_file):
+    """input: Center For Responsive Politics pac_2_pac text file
+       output: cleaned dataframe containing text file data
+     """
+
+    df = pd.read_csv(data_file,sep="\|*,\|", header=None)
+    df.columns = ['Cycle','FECRecNo','Filerid','DonorCmte','ContribLendTrans','City','State','Zip',
+                  'FECOccEmp','Primcode','Date,Amount,RecipID','Party','Otherid','RecipCode',
+                  'RecipPrimcode','Amend','Report','PG','Microfilm','Type','RealCode','Source']
+    return df
